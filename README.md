a375a6f7dd081dc222cdd49c32f5339fced7da06
Aniket-876 <aniketchaudhari6985@gmail.com>
2024-09-08 10:45:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculator
+This repo contains my project as Calculator using HTML5,CSS3and Javascript
---
+++ script.js
@@ -0,0 +1,34 @@
+let display = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let buttonArray = Array.from(buttons);
+let string = ' ';
+
+buttonArray.forEach(btn => {
+
+    btn.addEventListener('click',(e) => {
+
+        if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            display.value = string;
+
+        
+        }else if(e.target.innerHTML == 'AC'){
+             string =- ' ';
+             display.value = string;
+            
+            
+        }else if(e.target.innerHTML == '='){
+              string = eval(string);
+              display.value = string;
+              
+              
+        }else{
+
+            string += e.target.innerHTML;
+            display.value = string;
+
+        }
+     });
+
+});
